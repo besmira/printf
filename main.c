a762832e95ecf0df547759c6c80b4f8511c149ec
endritNovaku <ennald@outlook.com>
2022-03-14 05:21:10 -0700

    fixing betty and seperating it to functions

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - call _printf
+ * Return: 0
+ */
+int main(void)
+{
+	char *test = "test";
+	char *test2 = "readable";
+
+	_printf("testing c %s \n", test);
+	return (0);
+}
--- main.h
+++ main.h
@@ -1,4 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _printf(const char *form, ...);
+int checkArg(const char *str);
+int advPutchar(const char *form, char *str1, int j);
 #endif
