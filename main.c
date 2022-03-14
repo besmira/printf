6b97e4f147420f15340d3698d569777dd94721cd
Igli-Boci <igliboci10@gmail.com>
2022-03-14 13:33:47 -0700

    Retrying to finish the main function

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	_printf("This is a test test %d %c %% %s", -235, 't', "Another fuking test");
+
+	return (0);
+}
