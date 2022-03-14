8c41cc45fbe1fd3a4a10a08c3d42d29ec1566e8f
Igli-Boci <igliboci10@gmail.com>
2022-03-14 13:56:40 -0700

    Removed conflict files

--- main.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-int _printf(const char *form, ...);
-int checkArg(const char *str);
-int advPutchar(const char *form, char *str1, int j);
-void _putchar(char *str);
-#endif
