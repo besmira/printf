600180813aee3ef662e850c2ba691cdb4655a002
endritNovaku <ennald@outlook.com>
2022-03-15 02:42:46 -0700

    endri

--- main.c
+++ main.c
@@ -1,16 +1,26 @@
-#include "main.h"
+#include <limits.h>
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - call _printf
- * Return: 0
- */
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0
+ *     */
 int main(void)
 {
-	char *test = "test";
-	char test2 = 'c';
-	char *string = "this is a vary long string and it contains a long text";
+	int len;
+	int len2;
+	unsigned int ui;
+	void *addr;
 
-	_printf("testing c %% %s, %c, %s \n", test, test2, string);
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
+	len = _printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
 	return (0);
 }
