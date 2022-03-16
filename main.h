e3693e4df30fb59b6a3922fea0a815b63c3aaab0
Besi <amulibesmira95@gmail.com>
2022-03-16 11:00:10 +0100

    Create main.h

---
+++ main.h
@@ -0,0 +1,9 @@
+#include <stdarg.h>
+#ifndef MAIN_H
+#define MAIN_H
+int _printf(const char *form, ...);
+int pc(va_list arg);
+int pi(va_list arg);
+int ps(va_list arg);
+int _putchar(char c);
+#endif
